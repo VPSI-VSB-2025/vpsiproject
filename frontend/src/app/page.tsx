a9f24bb8a88d6263e00f646761b63c21a5350e75
Jan Bojko <honzabojko@seznam.cz>
2025-03-17 20:27:12 +0100

    CI/CD Netlify test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className='w-full h-screen flex items-center justify-center'>
-      <div className='font-bold text-3xl'>Nemocnice app - HomePage (test deployment)</div>
+      <div className='font-bold text-3xl'>Nemocnice app - HomePage (netlify deployment)</div>
     </div>
   )
 }
